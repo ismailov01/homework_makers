f6b0c0540df4090ea6cb080e862235b50aa47520
Kuba <kubaismailov02@gmail.com>
2021-10-27 19:24:48 +0600

    contactbook

---
+++ week-8/homework-react2/src/components/AddContact/index.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+
+const AddPost = ({ contacts, addContact }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const checkContactEmailExists = contacts.filter((contact) =>
+      contact.email === email ? contact : null
+    );
+    const checkContactPhoneExists = contacts.filter((contact) =>
+      contact.phone === phone ? contact : null
+    );
+
+    if (!email || !name || !phone) {
+      return toast.warning("Пожалуйста заполните все поля!!");
+    }
+    if (checkContactEmailExists.length > 0) {
+      return toast.error("Данные уже существуют!!");
+    }
+    if (checkContactPhoneExists.length > 0) {
+      return toast.error("Данные уже существуют!!");
+    }
+
+    const data = {
+      id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 0,
+      email,
+      name,
+      phone,
+    };
+
+    addContact(data);
+    toast.success("Контакт успешно добавлен!!");
+    history.push("/");
+  };
+
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center text-dark py-3 display-2">Добавить контакт</h1>
+      <div className="row">
+        <div className="col-md-6 p-5 mx-auto shadow">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Имя"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Фамилия"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="number"
+                placeholder="Номер телефона"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="btn btn-block btn-dark"
+                type="submit"
+                value="Добавить контакт"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  contacts: state,
+});
+const mapDispatchToProps = (dispatch) => ({
+  addContact: (data) => {
+    dispatch({ type: "ADD_CONTACT", payload: data });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPost);
